ce9bfe85f3c9d0a8bdd32787b2b4804caf75f5ce
Erik Noorland <mail@eriknoorland.nl>
2021-09-20 16:57:52 +0200

    Update example code

--- examples/basic.js
+++ examples/basic.js
@@ -1,24 +1,19 @@
 const Gripper = require('../index');
 const gripper = Gripper('/dev/tty.usbserial-14130');
 
-const jawCloseAngle = 35;
-const jawWideAngle = 85;
+const jawCloseAngle = 28;
 const jawOpenAngle = 125;
 const liftUpAngle = 75;
-const liftDownAngle = 120;
+const liftDownAngle = 140;
 
 async function onGripperInitialized() {
   await gripper.setJawAngle(jawOpenAngle);
   await gripper.setLiftAngle(liftDownAngle);
 
   setTimeout(async () => {
-    await gripper.setJawAngle(jawWideAngle);
-  }, 500);
-
-  setTimeout(async () => {
     await gripper.setJawAngle(jawCloseAngle);
     await gripper.setLiftAngle(liftUpAngle);
-  }, 1500);
+  }, 250);
 
   setTimeout(async () => {
     await gripper.setLiftAngle(liftDownAngle);
